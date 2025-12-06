27801ad39de118af2bf56878b607529a9856dd8b
Sola Yamini <yaminisola23@gmail.com>
2025-12-06 21:39:39 +0530

    Initialize mcqgenerator module with imports

---
+++ src/mcqgenerator/__init__.py
@@ -0,0 +1,4 @@
+# src/mcqgenerator/__init__.py
+from .MCQGenerator import MCQGenerator, generate_evaluate_chain, get_table_data
+
+__all__ = ['MCQGenerator', 'generate_evaluate_chain', 'get_table_data']
